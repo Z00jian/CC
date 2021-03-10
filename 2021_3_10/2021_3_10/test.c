a11794b97702befdb41e8b44c3c0ab5f92e79f55
Z00jian <1637209785@qq.com>
2021-03-10 22:31:17 +0800

    提交

---
+++ 2021_3_10/2021_3_10/test.c
@@ -0,0 +1,50 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//void test1()
+//{
+//	printf("hehe");
+//
+//}
+//void test(void (*p)())
+//{
+//	(*p)();
+//
+//}
+//int main()
+//{
+//	test(test1);
+//	return 0;
+//}
+//void boll(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz-1; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i; j++)
+//		if (arr[j]>arr[j+1])
+//		{
+//			int mp = arr[j];
+//			arr[j] = arr[j+1];
+//			arr[j+1] = mp;
+//		}
+//	}
+//}
+//void print(int arr[],int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d", arr[i]);
+//	}
+//	printf("\n");
+//	
+//}
+//int main()
+//{
+//	int arr[] = { 1, 5, 4, 3, 6, 9, 2, 8, 7 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	boll(arr,sz);
+//	print(arr,sz);
+//	return 0;
+//}
